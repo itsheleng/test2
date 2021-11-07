f1b342fefca6e14598c0778d80718922776d8991
itsheleng <93868012+itsheleng@users.noreply.github.com>
2021-11-07 16:12:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -19,4 +19,4 @@ st.sidebar.title("🔗 Sources")
 st.sidebar.info('[Given Data](https://www.data.gov.cy/)'+'\r'+'[Our World in Data](https://ourworldindata.org/)')
 
 st.sidebar.title("🛈 About") 
-st.sidebar.info('Created and maintained by:'+'\r'+'[Eleni Giakoumi](eg.giakoumi@edu.cut.ac.cy)')
+st.sidebar.info('Created and maintained by:'+'\r'+'[Eleni Giakoumi](eg.giakoumi@edu.cut.ac.cy)'+'[Andreas Othonos](am.othonos@edu.cut.ac.cy)')
