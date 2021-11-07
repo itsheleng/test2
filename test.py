2b3e411d5890d0e58cf33a3f16c74b6cac9b5f17
itsheleng <93868012+itsheleng@users.noreply.github.com>
2021-11-07 15:29:43 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# helloworld.py
+#import the library
+import streamlit as stl
+# add title
+stl.title(“CEI523 2021")
+#adding some text
+stl.write("Data Science is fun!")
+#adding a condition with checkbox
+if stl.checkbox(“Checked me please!”):
+st.balloons()
