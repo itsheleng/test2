68a4946300ae117189328665eb963469020b3081
itsheleng <93868012+itsheleng@users.noreply.github.com>
2021-11-07 17:16:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ st.text("")
 st.text("")
 
 df = pd.read_csv('https://drive.google.com/file/d/1cEMvten1WEJTae9xRw0JNezHrZZhhT9o/view?usp=sharing',error_bad_lines=False, delimiter=',', quotechar='"')
-#print(df)
-display(df)
+print(df)
+
 
 st.sidebar.text("")
 st.sidebar.text("")
