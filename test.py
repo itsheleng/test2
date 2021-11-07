05bcd74a4f74ac36b8717d704fbd28fd8e575bde
itsheleng <93868012+itsheleng@users.noreply.github.com>
2021-11-07 16:55:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import datetime
 import streamlit as stl
 # add title
 st.title('CEI 523 Assignment 2021 📈')
-st.info('Case study for our given data for predictive maintenance 📈')
+st.info('Case study for our given data for predictive maintenance')
 
 st.text("")
 st.text("")
