c5fedc3b86e77d7d5359f4a147b112f5bc9b11df
itsheleng <93868012+itsheleng@users.noreply.github.com>
2021-11-07 16:09:23 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,4 +13,10 @@ st.info('Case study for our given data for predictive maintenance')
 
 st.text("")
 st.text("")
+st.sidebar.text("")
+st.sidebar.text("")
+st.sidebar.title("🔗 Sources")
+st.sidebar.info('[Given Data](https://www.data.gov.cy/)'+'\r'+'[Our World in Data](https://ourworldindata.org/)')
 
+st.sidebar.title("🛈 About") 
+st.sidebar.info('Created and maintained by:'+'\r'+'[Eleni Giakoumi](eg.giakoumi@edu.cut.ac.cy)')
