e5fbb85456c7d4dd9a24828b71547db10a9da106
itsheleng <93868012+itsheleng@users.noreply.github.com>
2021-11-07 17:02:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -14,8 +14,6 @@ st.info('Case study for our given data for predictive maintenance')
 st.text("")
 st.text("")
 
-df = pd.read_csv('https://www.data.gov.cy/node/4844/download',error_bad_lines=False, delimiter=',', quotechar='"')
-display(df)
 
 st.sidebar.text("")
 st.sidebar.text("")
