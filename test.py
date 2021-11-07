ad7e6666ec018d007ee27e3b3636aea14b5f6016
itsheleng <93868012+itsheleng@users.noreply.github.com>
2021-11-07 17:21:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -15,8 +15,7 @@ st.text("")
 st.text("")
 
 df = pd.read_csv('https://www.data.gov.cy/node/4844/download',error_bad_lines=False, delimiter=',', quotechar='"')
-
-display(df)
+print(df)
 
 
 st.sidebar.text("")
